c4aa320fae655e6b14b2e21d7a05e41dfefc27f6
Zander <yoshifan090@gmail.com>
2024-10-10 18:51:38 -0500

    Added a test level

---
+++ levels/bob/header.inc.h
@@ -0,0 +1,14 @@
+extern const GeoLayout bob_area_1_geo[];
+extern const GeoLayout bob_area_1[];
+extern const Collision bob_area_1_collision[];
+extern const MacroObject bob_area_1_macro_objs[];
+extern Vtx bob_dl_Level_Mesh_mesh_layer_1_vtx_cull[8];
+extern Vtx bob_dl_Level_Mesh_mesh_layer_1_vtx_0[121];
+extern Gfx bob_dl_Level_Mesh_mesh_layer_1_tri_0[];
+extern Vtx bob_dl_Level_Mesh_mesh_layer_1_vtx_1[267];
+extern Gfx bob_dl_Level_Mesh_mesh_layer_1_tri_1[];
+extern Gfx mat_bob_dl_Grass_Example[];
+extern Gfx mat_revert_bob_dl_Grass_Example[];
+extern Gfx mat_bob_dl_f3dlite_material[];
+extern Gfx mat_revert_bob_dl_f3dlite_material[];
+extern Gfx bob_dl_Level_Mesh_mesh_layer_1[];
